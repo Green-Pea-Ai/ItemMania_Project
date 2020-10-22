813dcfb1c53e1ce8d7c0c68415ce1d6b46e0f26f
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-10-22 18:08:53 +0900

    201022 Thur 18:08,  clear git cache

--- 01_wd_Spring Project/src/main/java/com/example/wd/proj/service/ItemManiaService.java
+++ 01_wd_Spring Project/src/main/java/com/example/wd/proj/service/ItemManiaService.java
@@ -4,16 +4,17 @@ import com.example.wd.proj.entity.ItemManiaBoard;
 
 import java.util.List;
 
+// 인터페이스 부분
 public interface ItemManiaService {
-    // 인터페이스 부분
 
+    // 아이템 리스트 보기
     public List<ItemManiaBoard> maniaList() throws Exception;
-
+    // 아이템 등록
     public void maniaRegister(ItemManiaBoard board) throws Exception;
-
+    // 아이템 삭제
     public void removeFromServ(Integer custNo) throws Exception;
-
+    // 아이템 정보 읽기
     public ItemManiaBoard readFromServ(Integer custNo) throws Exception;
-
+    // 아이템 정보 수정
     public void modifyFromServ(ItemManiaBoard board) throws Exception;
 }
--- 01_wd_Spring Project/src/main/java/com/example/wd/proj/service/ItemManiaServiceImpl.java
+++ 01_wd_Spring Project/src/main/java/com/example/wd/proj/service/ItemManiaServiceImpl.java
@@ -13,26 +13,27 @@ public class ItemManiaServiceImpl implements ItemManiaService {
     @Autowired
     private ItemManiaRepository maniaRepository;
 
+    // 아이템 리스트 보기
     @Override
     public List<ItemManiaBoard> maniaList() throws Exception {
         return maniaRepository.mbList();
     }
-
+    // 아이템 등록
     @Override
     public void maniaRegister(ItemManiaBoard board) throws Exception {
         maniaRepository.itemInsert(board);
     }
-
+    // 아이템 삭제
     @Override
     public void removeFromServ(Integer custNo) throws Exception {
         maniaRepository.removeFromRepo(custNo);
     }
-
+    // 아이템 정보 보기
     @Override
     public ItemManiaBoard readFromServ(Integer custNo) throws Exception {
         return maniaRepository.readFromRepo(custNo);
     }
-
+    // 아이템 정보 수정
     @Override
     public void modifyFromServ(ItemManiaBoard board) throws Exception {
         maniaRepository.modifyFromRepo(board);
